265218208ed34edc05468fc79f3b375cc4701253
Matti Paksula <matti.paksula@iki.fi>
2016-05-05 23:40:51 +0300

    chore (commando) Assert harder.

--- spec/kommando_spec.rb
+++ spec/kommando_spec.rb
@@ -53,7 +53,7 @@ describe Kommando do
 
       it 'calls passes args correctly' do
         k = Kommando.new "head .rspec"
-        k.run
+        expect(k.run).to eq true
         expect(k.code).to eq 0
         # sleep 0.5 # TODO: bin/stress 10 100 makes this fail
         expect(k.out).to eq "--format documentation\r\n--color\r\n"
