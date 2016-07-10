1258ddc9097ac7c065e374a1d9b40164f28c12f9
Matti Paksula <matti.paksula@iki.fi>
2016-07-10 18:54:50 +0300

    chore (examples) Cleaned up .

--- examples/in_shell.rb
+++ examples/in_shell.rb
@@ -1,18 +1,16 @@
 require "./lib/kommando"
-require "tempfile"
 
-scratch = Tempfile.new
-
-k = Kommando.new "$ printf 'name:'; read N; printf \"lol, $N\"", {
-  output: true,
-  #latency: 0.5
+k = Kommando.new "$ printf 'name: '; read N; printf \"lol, $N\n\"", {
+  output: true
 }
 
-k.out.on "name:" do
-  k.in << "hello\n"
-  k.out.on "lol" do
-    puts "wat"
+k.out.on "name: " do
+  k.out.on /^lol, [^\n]+\r\n/ do
+    puts "Matched 'lol, USERINPUT\\r\\n' on stdout"
   end
+
+  k.in << "hello\n"
+
 end
 
 k.run
