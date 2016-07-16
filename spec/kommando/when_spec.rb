ea2bd6240ce29f9a83dde57206ac4616b252d35c
Matti Paksula <matti.paksula@iki.fi>
2016-07-16 09:01:58 +0300

    refact (specs) Organized k.when in describe blocks.

--- spec/kommando/when_spec.rb
+++ spec/kommando/when_spec.rb
@@ -2,46 +2,53 @@ require 'spec_helper'
 
 describe Kommando do
   describe 'when' do
-    it 'runs block when process has started' do
-      start_called = false
-      run_completed = false
 
-      k = Kommando.new "uptime"
-      k.when :start do
-        start_called = true
-        expect(run_completed).to be false
+    describe 'start' do
+      it 'runs block when process has started' do
+        start_called = false
+        run_completed = false
+
+        k = Kommando.new "uptime"
+        k.when :start do
+          start_called = true
+          expect(run_completed).to be false
+        end
+        k.run
+        run_completed = true
+
+        expect(start_called).to be true
       end
-      k.run
-      run_completed = true
-
-      expect(start_called).to be true
     end
 
-    it 'runs block when process has exited' do
-      exit_called = false
+    describe 'exit' do
+      it 'runs block when process has exited' do
+        exit_called = false
 
-      k = Kommando.new "uptime"
-      k.when :exit do
-        exit_called = true
+        k = Kommando.new "uptime"
+        k.when :exit do
+          exit_called = true
+        end
+        k.run
+
+        expect(exit_called).to be true
       end
-      k.run
-
-      expect(exit_called).to be true
     end
 
-    it 'runs block when process timeouts' do
-      timeout_called = false
+    describe 'timeout' do
+      it 'runs block when process timeouts' do
+        timeout_called = false
 
-      k = Kommando.new "sleep 10", {
-        timeout: 0.01
-      }
-      k.when :timeout do
-        timeout_called = true
+        k = Kommando.new "sleep 10", {
+          timeout: 0.01
+        }
+        k.when :timeout do
+          timeout_called = true
+        end
+
+        k.run
+
+        expect(timeout_called).to be true
       end
-
-      k.run
-
-      expect(timeout_called).to be true
     end
 
     it 'runs blocks given with strings' do
@@ -79,7 +86,7 @@ describe Kommando do
       end
     end
 
-    describe 'multiple' do
+    describe 'multiple blocks' do
       it 'runs multiple blocks when process has started' do
         start1_called = false
         start2_called = false
