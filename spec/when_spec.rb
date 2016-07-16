91d9e3e290ae99f2500fc42c83437b7e4b4adec1
Matti Paksula <matti.paksula@iki.fi>
2016-07-16 09:20:36 +0300

    chore (specs) Test When separately.

--- spec/when_spec.rb
+++ spec/when_spec.rb
@@ -6,4 +6,20 @@ describe Kommando::When do
       expect(Kommando::When.new).to be_an_instance_of Kommando::When
     end
   end
+
+  describe 'register and fire' do
+    it 'registers an event with a lambda and calls it' do
+      w = Kommando::When.new
+      registered = false
+
+      block = lambda do
+        registered = true
+      end
+      w.register :exit, block
+
+      expect(registered).to be false
+      w.fire :exit
+      expect(registered).to be true
+    end
+  end
 end
