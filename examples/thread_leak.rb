212ff23b8fcc1d02728c24b2925f5d832a623efe
Matti Paksula <matti.paksula@iki.fi>
2016-12-07 18:05:17 +0200

    chore (examples) More reliable thread leak test.

--- examples/thread_leak.rb
+++ examples/thread_leak.rb
@@ -1,22 +1,29 @@
 require "./lib/kommando"
 
-last_k = nil
+Kommando.timeout = 0.1
+Kommando.when :timeout do |k|
+  print "t"
+  puts k.inspect
+  exit 1
+end
+
 100.times do
-  last_k = Kommando.run "uptime", {
-    timeout: 0.1
-  }
-  last_k.when :timeout do
-    print "t"
-    puts last_k.out
-  end
+  Kommando.run "uptime"
   print "."
 end
 
-puts ""
+3.times do
+  unless Thread.list.count == 1
+    print "w(#{Thread.list.count})"
+    sleep 0.01
+  else
+    break
+  end
+end
+
 unless Thread.list.count == 1
   puts Thread.list.map(&:inspect).join("\n")
-  puts last_k.out
-  raise "Thread leak"
-
+  raise "Thread leak: #{Thread.list.count}"
 end
+
 puts "ok"
